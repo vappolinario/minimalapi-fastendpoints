98d43772353d4f6e4e73f10233e2860f8334cba6
Vitor Appolinario <vitor@appolinario.com>
2025-09-27 14:40:23 -0300

    feat: Pass the cancelation token to order service

--- OrdersAPI/Services/IOrderService.cs
+++ OrdersAPI/Services/IOrderService.cs
@@ -1,6 +1,6 @@
 public interface IOrderService
 {
-    public Task<Order> CreateOrderAsync(Order request);
-    public Task<IEnumerable<Order>> GetAllAsync();
-    public Task<Order?> GetByIdAsync(int id);
+    public Task<Order> CreateOrderAsync(Order request, CancellationToken cancellationToken = default);
+    public Task<IEnumerable<Order>> GetAllAsync(CancellationToken cancellationToken = default);
+    public Task<Order?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
 }
--- OrdersAPI/Services/OrderService.cs
+++ OrdersAPI/Services/OrderService.cs
@@ -9,21 +9,21 @@ public class OrderService : IOrderService
         _context = context;
     }
 
-    public async Task<Order> CreateOrderAsync(Order request)
+    public async Task<Order> CreateOrderAsync(Order request, CancellationToken cancellationToken = default)
     {
-        await _context.Orders.AddAsync(request);
-        await _context.SaveChangesAsync();
+        await _context.Orders.AddAsync(request, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
         return request;
     }
 
-    public async Task<IEnumerable<Order>> GetAllAsync()
+    public async Task<IEnumerable<Order>> GetAllAsync(CancellationToken cancellationToken = default)
     {
-        return await _context.Orders.ToListAsync();
+        return await _context.Orders.ToListAsync(cancellationToken);
     }
 
-    public async Task<Order?> GetByIdAsync(int id)
+    public async Task<Order?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
     {
-        return await _context.Orders.FirstOrDefaultAsync(o => o.Id == id);
+        return await _context.Orders.FirstOrDefaultAsync(o => o.Id == id, cancellationToken);
     }
 }
 
